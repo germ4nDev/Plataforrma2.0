c7b85574b8b32a1eb96f88c59ab8a05ea28f7a1f
GermanDev320 <german.valencia@outlook.com>
2025-12-13 19:11:19 -0500

    ajustes

--- src/app/theme/shared/_helpers/error.interceptor.ts
+++ src/app/theme/shared/_helpers/error.interceptor.ts
@@ -7,16 +7,15 @@ import { AuthenticationService } from '../service/authentication.service';
 
 @Injectable()
 export class ErrorInterceptor implements HttpInterceptor {
+
   constructor(private authenticationService: AuthenticationService) {}
 
   intercept(request: HttpRequest<string>, next: HttpHandler): Observable<HttpEvent<string>> {
     return next.handle(request).pipe(
       catchError((err) => {
         if (err.status === 401) {
-          // auto logout if 401 response returned from api
           this.authenticationService.logout();
         }
-
         const error = err.error.message || err.statusText;
         return throwError(error);
       })
